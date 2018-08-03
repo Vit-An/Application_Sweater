e814579896844120ec7ee8b55dbab9213dca3869
Vitaliy Bigun <skil.smile@gmail.com>
2018-08-03 14:59:45 +0300

    Add captcha check. Spring session and rememberMe.

--- src/main/java/com/sweater/service/UserService.java
+++ src/main/java/com/sweater/service/UserService.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.*;
 import java.util.stream.Collectors;
 
