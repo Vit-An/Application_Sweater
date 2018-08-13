9179c98f1b439faedc715002ae5994354bdec539
Vitaliy Bigun <skil.smile@gmail.com>
2018-08-13 14:55:17 +0300

    Add subscriptions

--- src/main/java/com/sweater/controller/UserController.java
+++ src/main/java/com/sweater/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sweater.controller;
 
 import com.sweater.model.Role;
 import com.sweater.model.User;
+import com.sweater.repos.UserRepo;
 import com.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -64,6 +65,41 @@ public class UserController {
        return "redirect:/user/profile";
     }
 
+    @GetMapping("subscribe/{user}")
+    public String subscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ){
+       userService.subscribe(currentUser, user);
+       return "redirect:/user-messages/"+ user.getId();
+    }
+
+    @GetMapping("unsubscribe/{user}")
+    public String unsubscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ){
+       userService.unsubscribe(currentUser, user);
+       return "redirect:/user-messages/"+ user.getId();
+    }
+
+    @GetMapping("{type}/{user}/list")
+    public String userList(
+             Model model,
+             @PathVariable User user,
+             @PathVariable String type
+    ){
+       model.addAttribute("userChannel", user);
+       model.addAttribute("type", type);
+
+       if ("subscriptions".equals(type)){
+           model.addAttribute("users", user.getSubscriptions());
+       } else{
+           model.addAttribute("users", user.getSubscribers());
+       }
+
+       return "subscriptions";
+    }
 
 
 
